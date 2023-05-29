e6649e8283f2fd0e1c880816ba3e3ff0ae16feb7
chaffexd <shanechaffe@gmail.com>
2023-05-29 15:11:50 +0100

    now fetches data from firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { uiActions } from './store/ui-slice';
 
 import Notification from './components/UI/Notification';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { sendCartData, fetchCartData } from './store/cart-actions';
 
 let isInitial = true;
 
@@ -15,44 +15,55 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
+  // method 2 if you move actions into redux
   useEffect(() => {
-    const sendCartData = async() => {
-      dispatch(uiActions.showNotification({ 
-        status: 'pending',
-        title: 'Sending',
-        message: 'Sending cart data...'
-        })
-      );
-      const response = await fetch('https://redux-backend-ba488-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
-        // put overrides existing data where as post adds in a list
-        method: 'PUT',
-        body: JSON.stringify(cart)
-      });
-      if(!response.ok) {
-        throw new Error('Sending cart data failed.')
-      }
+    dispatch(fetchCartData())
+  }, [dispatch]);
 
-      dispatch(uiActions.showNotification({ 
-        status: 'success',
-        title: 'Success!',
-        message: 'Sent cart data successfully!'
-        })
-      );
-    };
+  useEffect(() => {
+    // this approach allows the set up in the component, I have also moved it into the cart-slice
 
-    // prevents sendCartData executing on page load
+    // const sendCartData = async() => {
+    //   dispatch(uiActions.showNotification({ 
+    //     status: 'pending',
+    //     title: 'Sending',
+    //     message: 'Sending cart data...'
+    //     })
+    //   );
+    //   const response = await fetch('https://redux-backend-ba488-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+    //     // put overrides existing data where as post adds in a list
+    //     method: 'PUT',
+    //     body: JSON.stringify(cart)
+    //   });
+    //   if(!response.ok) {
+    //     throw new Error('Sending cart data failed.')
+    //   }
+
+    //   dispatch(uiActions.showNotification({ 
+    //     status: 'success',
+    //     title: 'Success!',
+    //     message: 'Sent cart data successfully!'
+    //     })
+    //   );
+    // };
+
+    // // prevents sendCartData executing on page load
     if(isInitial) {
       isInitial = false;
       return;
-    }
+    };
 
-    sendCartData().catch(error => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error',
-        message: 'Sending cart data failed.'
-      }))
-    });
+    if(cart.changed) {
+      dispatch(sendCartData(cart));
+    };
+
+    // sendCartData().catch(error => {
+    //   dispatch(uiActions.showNotification({
+    //     status: 'error',
+    //     title: 'Error',
+    //     message: 'Sending cart data failed.'
+    //   }))
+    // });
   }, [cart, dispatch]);
 
   return (
